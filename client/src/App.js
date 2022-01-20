ee83bb6dcd55a9c983b8df21df53c97dd1a6c634
Rus <81392804+koparion@users.noreply.github.com>
2022-01-20 18:02:57 -0500

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import React from "react";
-import Input from './components/Input'
-import Lists from './components/Lists';
+import "./App.css";
+import React, { useState } from "react";
+import Home from "./components/Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+// import Routing from "./components/Routes";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
 
 function App() {
+  //main routing is set in App
+  const [lockin, setLockin] = useState(false); //preventing page viewing without login
   return (
     <>
-      <div className='container'>
-        <Input />
-        <Lists />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setLockin={setLockin}/>} /> {/* passing blocked page unless logged in*/}
+          <Route path="/register" element={<Register />} />
+          {lockin && <Route path="/profile" element={<Profile />}/>}
+        </Routes>
+      </BrowserRouter>
+      {/* <Routing/> */}
     </>
   );
 }
